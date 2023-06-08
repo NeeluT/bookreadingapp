67d03cd3b9c341458b0dbbe584de037b9461b1ba
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-06-08 17:02:39 -0400

    styling by habib, coauthored by matt, neelu & alina

--- main_app/data.py
+++ main_app/data.py
@@ -1,55 +1,55 @@
 def data():
     return [
         {
-            "ID": "0001",
+            
             "title": "Goodnight Moon",
             "author": "Margaret Wise Brown",
-            "image_url": "https://cdn.shopify.com/s/files/1/0126/8596/7418/products/image_ccd24dec-ff7c-4af6-9cf4-75af2b52c52b_1200x1200.jpg?v=1642883549",
+            "image_url": "https://m.media-amazon.com/images/I/91WuHblNkEL._AC_UF1000,1000_QL80_.jpg",
             "description": "A soothing bedtime story that follows a young rabbit saying goodnight to everything around.",
             "age_group": "A"
         },
         {
-            "ID": "0002",
+            
             "title": "The Very Hungry Caterpillar",
             "author": "Eric Carle",
-            "image_url": "https://example.com/book2.jpg",
+            "image_url": "https://m.media-amazon.com/images/I/91vnzZO5yPL._AC_UF1000,1000_QL80_.jpg",
             "description": "Follows the journey of a caterpillar as it eats its way through various foods before transforming into a butterfly.",
             "age_group": "B"
         },
         {
-            "ID": "0003",
+            
             "title": "Brown Bear, Brown Bear, What Do You See?",
             "author": "Bill Martin Jr.",
-            "image_url": "https://example.com/book3.jpg",
+            "image_url": "https://mpd-biblio-covers.imgix.net/9781627797238.jpg",
             "description": "Introduces various animals and colors through repetitive text and vibrant illustrations.",
             "age_group": "B"
         },
         {
-            "ID": "0004",
+          
             "title": "Guess How Much I Love You",
             "author": "Sam McBratney",
-            "image_url": "https://example.com/book4.jpg",
+            "image_url": "https://m.media-amazon.com/images/I/814PN+T0+pL._AC_UF1000,1000_QL80_.jpg",
             "description": "A story about the love between a parent and child, featuring two nutbrown hares.",
             "age_group": "C"
         },
         {
-            "ID": "0005",
+          
             "title": "Where the Wild Things Are",
             "author": "Maurice Sendak",
-            "image_url": "https://example.com/book5.jpg",
+            "image_url": "https://m.media-amazon.com/images/I/A1wx+e0rJ2L._AC_UF1000,1000_QL80_.jpg",
             "description": "Max journeys to an imaginary world where he becomes king of the wild things.",
             "age_group": "E"
         },
         {
-            "ID": "0006",
+            
             "title": "The Cat in the Hat",
             "author": "Dr. Seuss",
-            "image_url": "https://example.com/book6.jpg",
+            "image_url": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1182566765i/1296690.jpg",
             "description": "Two children's rainy day is turned upside down when the mischievous Cat in the Hat pays them a visit.",
             "age_group": "D"
         },
         {
-            "ID": "0007",
+            
             "title": "Corduroy",
             "author": "Don Freeman",
             "image_url": "https://example.com/book7.jpg",
@@ -57,26 +57,26 @@ def data():
             "age_group": "D"
         },
         {
-            "ID": "0008",
+           
             "title": "The Giving Tree",
             "author": "Shel Silverstein",
-            "image_url": "https://example.com/book8.jpg",
+            "image_url": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347517273i/231850.jpg",
             "description": "Follows the relationship between a young boy and a tree that selflessly provides for him throughout his life.",
             "age_group": "E"
         },
         {
-            "ID": "0009",
+            
             "title": "Green Eggs and Ham",
             "author": "Dr. Seuss",
-            "image_url": "https://example.com/book9.jpg",
+            "image_url": "https://cdn.waterstones.com/bookjackets/large/9780/0071/9780007133178.jpg",
             "description": "A story of Sam-I-Am's persistence in trying to get another character to try green eggs and ham.",
             "age_group": "C"
         },
         {
-            "ID": "0010",
+           
             "title": "The Little Engine That Could",
             "author": "Watty Piper",
-            "image_url": "https://example.com/book10.jpg",
+            "image_url": "https://www.nationaltrust.org.au/wp-content/uploads/2023/03/the-little-engine-that-could.jpg",
             "description": "A little engine overcomes challenges and believes in itself to successfully pull a train over a mountain.",
             "age_group": "A"
         }
